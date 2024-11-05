f6d7216536a2eabadc20474ee92d9017d0b5227c
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-05 15:26:49 +0100

    aggiunta ciclo for per creare 10 e-mail casuali

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then((res) => {
-    //console.log(res.data)
 
-    const randomEmail = res.data.response
-    console.log(randomEmail)
-})
-.catch((err) => {
-    console.log(err)
-})
+for (i = 0; i < 10; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+            //console.log(res.data)
 
-
+            const randomEmail = res.data.response
+            console.log(randomEmail)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
