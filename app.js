bd657aa6c7552a1def1a0a29c0245d68d93afc7e
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-05 16:59:35 +0100

    creazione bottone html

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const emailList = document.getElementById('email-list')
+const refreshButton = document.getElementById('refresh-button')
+
 //console.log(emailList)
+//console.log(refreshButton)
 
 for (i = 0; i < 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
@@ -13,13 +16,22 @@ for (i = 0; i < 10; i++) {
             <ul class="list-group">
             <li class="list-group-item list-group-item-secondary">${randomEmail}</li>
             </ul>`
+
+            
+
      
         })
         .catch((err) => {
             console.log(err)
         })
-
-     
-        
+      
 }
+
+
+refreshButton.addEventListener ('click', function(){
+    
+        
+
+  
+})
 
